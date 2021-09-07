418ae20ea8e0b337f64130284a8d972e3cba1bca
Logeshwaran <logeshsiva395@gmail.com>
2021-09-07 22:05:05 +0530

    project section

---
+++ script.js
@@ -0,0 +1,9 @@
+function fun1(){
+    document.getElementById("main").style.background='linear-gradient( to right bottom,rgba(255, 255, 255, 0.8),rgba(255, 255, 255, 0.3))';
+}
+function fun2(){
+ document.getElementById("main").style.background='linear-gradient( to right bottom,rgba(255, 255, 255, 0.8),rgba(255, 255, 255, 0.3))';
+}
+function fun3(){
+ document.getElementById("main").style.background='linear-gradient(to right bottom,rgba(238, 116, 116, 0.8),rgb(64, 153, 175))';
+}
